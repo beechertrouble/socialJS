037c552efac69fb89191c5d922c7dbad23663dce
beecher <smithstackhouse@radicalmedia.com>
2013-02-08 18:03:52 -0500

    improvements again - pinterest will no longer parse if there is no IMG set

--- socialJS.js
+++ socialJS.js
@@ -213,7 +213,7 @@ var socialJS = function(target, parseNow, args) {
 												
 				if(typeof gapi !== 'undefined') { // && typeof window.googleapis !== 'undefined'
 
-					setTimeout(function() { gapi.plusone.go(); }, 700);
+					setTimeout(function() { gapi.plusone.go(); }, 900); // having to put a stupid delay on this to deal with the intermittant window.googleapis error ... 
 
 				} else {
 				
@@ -247,38 +247,42 @@ var socialJS = function(target, parseNow, args) {
 	this.pinterest = {
 		
 		layout : false,
-		media : false,
 		description : false,
 		html : function(S) {
 				
-				var layout = S.pinterest.layout ? ' count-layout="' + S.pinterest.layout + '" ' : ' count-layout="horizontal" ';
-				var count = S.showCount ? layout : ' count-layout="none" ';
-				var media = S.IMG !== undefined && S.pinterest.media === false ? S.IMG : S.pinterest.media;
-					media = media !== undefined && media !== false ? '&media=' + encodeURIComponent( media ) : '';
-				var gobalText = S.description ?  '&description=' + encodeURIComponent( S.description ) : '';
-				var desc = S.pinterest.description ? '&description=' + encodeURIComponent( S.pinterest.description ) : gobalText;
-				var theHTML = '<a href="http://pinterest.com/pin/create/button/?url=' + encodeURIComponent( S.URL ) + media + '" class="pin-it-button" ' + count + '><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>'; 
+				if(S.IMG !== undefined) {
 				
-				S.target.find(".pinterest_wrap").html(theHTML).addClass('social_inited');
+					var layout = S.pinterest.layout ? ' count-layout="' + S.pinterest.layout + '" ' : ' count-layout="horizontal" ';
+					var count = S.showCount ? layout : ' count-layout="none" ';
+					var media = S.IMG !== undefined ? '&media=' + encodeURIComponent( media ) : '';
+					var gobalText = S.description ?  '&description=' + encodeURIComponent( S.description ) : '';
+					var desc = S.pinterest.description ? '&description=' + encodeURIComponent( S.pinterest.description ) : gobalText;
+					var theHTML = '<a href="http://pinterest.com/pin/create/button/?url=' + encodeURIComponent( S.URL ) + media + '" class="pin-it-button" ' + count + '><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>'; 
+					
+					S.target.find(".pinterest_wrap").html(theHTML).addClass('social_inited');
+				
+				}
 				
 			},
 		script : function(S) {
-				S.pinterest.parse();
+				S.pinterest.parse(S);
 			},
 		parse : function(S) {
+						
+			if(S.IMG !== undefined) {
 			
-			(function(m,q,c){var s=function(h){var d=c.pinit,n="?",a,i,f,b;f=[];b=[];var j={},g=m.createElement("IFRAME"),r=h.getAttribute(c.att.count)||false,o=h.getAttribute(c.att.layout)||"horizontal";if(q.location.protocol==="https:")d=c.pinit_secure;f=h.href.split("?")[1].split("#")[0].split("&");a=0;for(i=f.length;a<i;a+=1){b=f[a].split("=");j[b[0]]=b[1]}a=f=0;for(i=c.vars.req.length;a<i;a+=1){b=c.vars.req[a];if(j[b]){d=d+n+b+"="+j[b];n="&"}f+=1}if(j.media&&j.media.indexOf("http")!==0)f=0;if(f===i){a=0;
-for(i=c.vars.opt.length;a<i;a+=1){b=c.vars.opt[a];if(j[b])d=d+n+b+"="+j[b]}d=d+"&layout="+o;d=d+"&ref="+encodeURIComponent(m.URL);if(r!==false)d+="&count=1";g.setAttribute("src",d);g.setAttribute("scrolling","no");g.allowTransparency=true;g.frameBorder=0;g.style.border="none";g.style.width=c.layout[o].width+"px";g.style.height=c.layout[o].height+"px";h.parentNode.replaceChild(g,h)}else h.parentNode.removeChild(h)},p=m.getElementsByTagName("A"),l,e,k=[];e=0;for(l=p.length;e<l;e+=1)k.push(p[e]);e=0;
-for(l=k.length;e<l;e+=1)k[e].href&&k[e].href.indexOf(c.button)!==-1&&s(k[e])})(document,window,{att:{layout:"count-layout",count:"always-show-count"},pinit:"http://pinit-cdn.pinterest.com/pinit.html",pinit_secure:"https://assets.pinterest.com/pinit.html",button:"//pinterest.com/pin/create/button/?",vars:{req:["url","media"],opt:["title","description"]},layout:{none:{width:43,height:20},vertical:{width:43,height:58},horizontal:{width:90,height:20}}});
+				(function(m,q,c){var s=function(h){var d=c.pinit,n="?",a,i,f,b;f=[];b=[];var j={},g=m.createElement("IFRAME"),r=h.getAttribute(c.att.count)||false,o=h.getAttribute(c.att.layout)||"horizontal";if(q.location.protocol==="https:")d=c.pinit_secure;f=h.href.split("?")[1].split("#")[0].split("&");a=0;for(i=f.length;a<i;a+=1){b=f[a].split("=");j[b[0]]=b[1]}a=f=0;for(i=c.vars.req.length;a<i;a+=1){b=c.vars.req[a];if(j[b]){d=d+n+b+"="+j[b];n="&"}f+=1}if(j.media&&j.media.indexOf("http")!==0)f=0;if(f===i){a=0;
+	for(i=c.vars.opt.length;a<i;a+=1){b=c.vars.opt[a];if(j[b])d=d+n+b+"="+j[b]}d=d+"&layout="+o;d=d+"&ref="+encodeURIComponent(m.URL);if(r!==false)d+="&count=1";g.setAttribute("src",d);g.setAttribute("scrolling","no");g.allowTransparency=true;g.frameBorder=0;g.style.border="none";g.style.width=c.layout[o].width+"px";g.style.height=c.layout[o].height+"px";h.parentNode.replaceChild(g,h)}else h.parentNode.removeChild(h)},p=m.getElementsByTagName("A"),l,e,k=[];e=0;for(l=p.length;e<l;e+=1)k.push(p[e]);e=0;
+	for(l=k.length;e<l;e+=1)k[e].href&&k[e].href.indexOf(c.button)!==-1&&s(k[e])})(document,window,{att:{layout:"count-layout",count:"always-show-count"},pinit:"http://pinit-cdn.pinterest.com/pinit.html",pinit_secure:"https://assets.pinterest.com/pinit.html",button:"//pinterest.com/pin/create/button/?",vars:{req:["url","media"],opt:["title","description"]},layout:{none:{width:43,height:20},vertical:{width:43,height:58},horizontal:{width:90,height:20}}});
+	
+			}
 			
 		}
 		
 	} //
 
-	this.init = function() {
-		
-		var S = this;
-						
+	this.init = function(S) {
+								
 		for(var i=0;i<S.which.length;i++) {
 			
 			var which = S.which[i];
@@ -316,9 +320,9 @@ for(l=k.length;e<l;e+=1)k[e].href&&k[e].href.indexOf(c.button)!==-1&&s(k[e])})(d
 	this.parseEm = function(S) {
 				
 		var S = S === undefined ? this : S;
-		
+				
 		if(!S.target.is('.social_inited')) 
-			S.init();
+			S.init(S);
 		
 		switch(window.socialScriptsStatus) {
 			
@@ -334,13 +338,13 @@ for(l=k.length;e<l;e+=1)k[e].href&&k[e].href.indexOf(c.button)!==-1&&s(k[e])})(d
 				break;
 				
 			case('loading'):
-				setTimeout(function() { S.parseEm(S); }, 500);
+				setTimeout(function() { S.parseEm(); }, 500);
 				break;
 				
 			case(undefined):
 			default:
 				S.getScripts();
-				setTimeout(function() { S.parseEm(S); }, 750);
+				setTimeout(function() { S.parseEm(); }, 750);
 				break;
 			
 		}
@@ -353,7 +357,7 @@ for(l=k.length;e<l;e+=1)k[e].href&&k[e].href.indexOf(c.button)!==-1&&s(k[e])})(d
 		
 		S.URL = newURL;
 		S.target.find(".socail_wrap").empty();
-		S.init();
+		S.init(S);
 		S.parseEm();
 		
 	} //
