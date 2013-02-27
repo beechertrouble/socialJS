c43904ee19f462c481a7f6b3d2c8947ed3d9272f
beecher <smithstackhouse@radicalmedia.com>
2013-02-27 11:41:56 -0500

    fixed for the changes that pinterest decided to make

--- socialJS.js
+++ socialJS.js
@@ -1,5 +1,5 @@
 /**
-* v. 3.6
+* v. 3.7
 * 
 */
 var socialJS = function(target, parseNow, args) {
@@ -398,12 +398,12 @@ var socialJS = function(target, parseNow, args) {
 								
 				if(S.IMG !== undefined) {
 								
-					var layout = S.pinterest.layout ? ' count-layout="' + S.pinterest.layout + '" ' : ' count-layout="horizontal" ',
-						count = S.showCount ? layout : ' count-layout="none" ',
+					var layout = S.pinterest.layout ? ' data-pin-config="' + S.pinterest.layout + '" ' : ' data-pin-config="beside" ',
+						count = S.showCount ? layout : ' data-pin-config="none" ',
 						media = S.IMG !== undefined && S.IMG != 'undefined' ? '&media=' + encodeURIComponent( S.IMG ) : '',
 						gobalText = S.description ?  '&description=' + encodeURIComponent( S.description ) : '',
-						desc = S.pinterest.description ? '&description=' + encodeURIComponent( S.pinterest.description ) : gobalText,
-						theHTML = '<a href="http://pinterest.com/pin/create/button/?url=' + encodeURIComponent( S.URL ) + media + '" class="pin-it-button" ' + count + '><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>'; 
+						desc = S.pinterest.description ? '&description=' + encodeURIComponent( S.pinterest.description ) : '&description=' + gobalText,
+						theHTML = '<a ' + count + ' href="//pinterest.com/pin/create/button/?url=' + encodeURIComponent( S.URL ) + media + desc + '" data-pin-do="buttonPin" data-pin-log="button_pinit" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>';
 										
 					S.target.find(".pinterest_wrap").html(theHTML).addClass('social_inited');
 				
@@ -417,10 +417,8 @@ var socialJS = function(target, parseNow, args) {
 			
 			if(SOC.IMG !== undefined) {
 			
-				(function(m,q,c){var s=function(h){var d=c.pinit,n="?",a,i,f,b;f=[];b=[];var j={},g=m.createElement("IFRAME"),r=h.getAttribute(c.att.count)||false,o=h.getAttribute(c.att.layout)||"horizontal";if(q.location.protocol==="https:")d=c.pinit_secure;f=h.href.split("?")[1].split("#")[0].split("&");a=0;for(i=f.length;a<i;a+=1){b=f[a].split("=");j[b[0]]=b[1]}a=f=0;for(i=c.vars.req.length;a<i;a+=1){b=c.vars.req[a];if(j[b]){d=d+n+b+"="+j[b];n="&"}f+=1}if(j.media&&j.media.indexOf("http")!==0)f=0;if(f===i){a=0;
-	for(i=c.vars.opt.length;a<i;a+=1){b=c.vars.opt[a];if(j[b])d=d+n+b+"="+j[b]}d=d+"&layout="+o;d=d+"&ref="+encodeURIComponent(m.URL);if(r!==false)d+="&count=1";g.setAttribute("src",d);g.setAttribute("scrolling","no");g.allowTransparency=true;g.frameBorder=0;g.style.border="none";g.style.width=c.layout[o].width+"px";g.style.height=c.layout[o].height+"px";h.parentNode.replaceChild(g,h)}else h.parentNode.removeChild(h)},p=m.getElementsByTagName("A"),l,e,k=[];e=0;for(l=p.length;e<l;e+=1)k.push(p[e]);e=0;
-	for(l=k.length;e<l;e+=1)k[e].href&&k[e].href.indexOf(c.button)!==-1&&s(k[e])})(document,window,{att:{layout:"count-layout",count:"always-show-count"},pinit:"http://pinit-cdn.pinterest.com/pinit.html",pinit_secure:"https://assets.pinterest.com/pinit.html",button:"//pinterest.com/pin/create/button/?",vars:{req:["url","media"],opt:["title","description"]},layout:{none:{width:43,height:20},vertical:{width:43,height:58},horizontal:{width:90,height:20}}});
-								
+				$.getScript('http://assets.pinterest.com/js/pinit.js');
+					
 			}
 			
 		}
